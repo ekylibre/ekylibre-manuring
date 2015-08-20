75b47cab0247696759d24ec4e18e4e89d8ae3020
Brice TEXIER <brice@ekylibre.com>
2015-08-20 22:50:50 +0200

    Retrieves unmaintained MMP methods with its abaci

---
+++ lib/manuring/abaci.rb
@@ -0,0 +1,21 @@
+module Manuring
+  module Abaci
+
+    class << self
+      def root
+        Pathname.new(__FILE__).dirname.dirname.dirname.join("db", "abaci")
+      end
+      
+      def load_files
+        Dir.glob(root.join("*.csv")) do |path|
+          name = File.basename(path, ".csv")
+          puts "Set #{name.classify} with #{path}".red
+          const_set(name.classify, ::Abaci.read(path))
+        end
+      end
+    end
+
+    # Load all abaci in one time
+    load_files
+  end
+end
