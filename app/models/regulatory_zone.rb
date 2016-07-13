62191663232f6882f8efdfa0219b7a1f007587e2
jleveau <jleveau@ekylibre.com>
2016-07-13 12:03:25 +0200

    unified shape in regulatory zone

--- app/models/regulatory_zone.rb
+++ app/models/regulatory_zone.rb
@@ -1,6 +1,12 @@
 
 class RegulatoryZone  < Ekylibre::Record::Base
 
+  def self.unified_shape
+    ActiveRecord::Base.connection.execute("SELECT ST_Union(RZ.shape)
+                                            FROM regulatory_zones RZ
+                                            Where type = '#{self.name}' ;").first.values.first
+  end
+
   def self.build_non_spreadable_zone(manure_management_plan,options={})
     #Build the intersection between non spreadable zones and the zones from manure_management_plan
     # return a hash {shape : <regulatory_zone>,
@@ -19,23 +25,23 @@ class RegulatoryZone  < Ekylibre::Record::Base
      the option hash
 =end
     ActiveRecord::Base.logger = nil
-    non_spreadable_zone_shape = (ActiveRecord::Base.connection.execute "SELECT ST_AsEWKT(ST_Union(ST_Intersection(
+    non_spreadable_zone_shape = (ActiveRecord::Base.connection.execute "SELECT ST_Union(ST_Intersection(
 
                                                                   ST_Buffer(RZ.shape::geography, CASE RZ.type
                                                                                             WHEN 'Watercourse' THEN #{options[:watercourse_buffer]}
                                                                                             WHEN 'BodyOfWater' THEN #{options[:bodyofwater_buffer]}
-                                                                                            WHEN 'VulnerableZone' THEN #{options[:vulnerablezone_buffer]}
                                                                                             ELSE #{ options[:default]}
-                                                                                        END)::geometry, AP.support_shape)))
+                                                                                        END)::geometry, AP.support_shape))
                                                   FROM regulatory_zones as RZ
                                                   JOIN activity_productions as AP on RZ.shape && AP.support_shape
-                                                   WHERE AP.campaign_id = 4
+                                                   WHERE AP.campaign_id = 4 AND RZ.type IN ('BodyOfWater','Watercourse')
                                                   ;", manure_management_plan.campaign.id).first.values.first
 
+
     non_spreadable_charta = Charta.new_geometry(non_spreadable_zone_shape)
+    #compute square meters area
     manure_management_plan.zones.each do |zone|
-      zone_charta = Charta.new_geometry(zone.shape)
-      info[:areas][zone.id] =  zone_charta.difference(non_spreadable_charta).area.in(Nomen::Unit.find(:hectare)).round(2)
+      info[:areas][zone.id] =  Charta.new_geometry(zone.support_shape).difference(non_spreadable_charta).area.in(Nomen::Unit.find(:hectare)).round(2)
     end
 
     return [non_spreadable_charta,info]
