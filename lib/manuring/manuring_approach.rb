9a82b00054af6027dda56bd867de5003e5356d93
David JOULIN <ionosphere.consulting@gmail.com>
2016-08-17 17:05:40 +0200

    Update animal method

--- lib/manuring/manuring_approach.rb
+++ lib/manuring/manuring_approach.rb
@@ -32,11 +32,19 @@ module Manuring
       end
       
       
+      def animal_output
+        
+        animals = Animal.at(@opened_at)
+        
+        return daily_nitrogen_production(animals)
+        
+      end
+      
       # compute average annual milk production per animal from annual milk production
       def average_annual_milk_animal_production_from_milk_production
         # count all animal producing milkin the period
         animal_milk_member_count = []
-        animal_milk_member_count << 1
+        animal_milk_member_count << 55
         
         if @milk_annual_production_from_all_adult_female_cow
           (@milk_annual_production_from_all_adult_female_cow / animal_milk_member_count.compact.sum) / 0.92
@@ -59,17 +67,18 @@ module Manuring
           
           # set variables with default values
           quantity = 0.in_kilogram_per_day
-          animal_milk_production = 0
-          animal_age = 24
+          animal_milk_production = nil
+          
           
           # get data
           # age (if born_at not present then animal has 24 month)
+          animal_age = 24
           if animal.age
             animal_age = (animal.age / (3600*24*30)).to_d
           end
           
           # production (if a cow, get annual milk production)
-          if Nomen::Varieties[animal.variety] <= :bos and animal.can("produce(milk)")
+          if Nomen::Varieties[animal.variety] <= :bos and animal.able_to?("produce(milk)")
             if avg_milk
               animal_milk_production = avg_milk
             elsif animal.milk_daily_production
@@ -77,14 +86,22 @@ module Manuring
             end
           end
           
-          items = Manuring::Abaci::NmpFranceAnimalNitrogenProduction.list.select do |item|
-            item.minimum_age <= animal_age.to_i and
-            animal_age.to_i < item.maximum_age and
-            item.minimum_milk_production <= animal_milk_production.to_i and
-            animal_milk_production.to_i < item.maximum_milk_production and
-            @external_building_attendance_in_month_from_all_adult_female_cow.to_d < item.maximum_outside_presence and
-            @external_building_attendance_in_month_from_all_adult_female_cow.to_d > item.minimum_outside_presence.to_d and
-            item.variant.to_s == animal.variant.reference_name.to_s
+          if animal_milk_production && animal_age && @external_building_attendance_in_month_from_all_adult_female_cow
+            items = Manuring::Abaci::NmpFranceAnimalNitrogenProduction.select do |item|
+              item.minimum_age <= animal_age.to_i and
+              animal_age.to_i < item.maximum_age and
+              item.minimum_milk_production <= animal_milk_production.to_i and
+              animal_milk_production.to_i < item.maximum_milk_production and
+              @external_building_attendance_in_month_from_all_adult_female_cow.to_d < item.maximum_outside_presence and
+              @external_building_attendance_in_month_from_all_adult_female_cow.to_d > item.minimum_outside_presence.to_d and
+              item.variant.to_s == animal.variant.reference_name.to_s
+            end
+          elsif animal_age
+            items = Manuring::Abaci::NmpFranceAnimalNitrogenProduction.select do |item|
+                item.minimum_age <= animal_age.to_i and
+                animal_age.to_i < item.maximum_age and
+                item.variant.to_s == animal.variant.reference_name.to_s
+            end
           end
           
           if items.any?
